ab98f67b41daa12f4f90f3a0f7a5bd3a59a3147e
W-yathtl <dopin9panda@gmail.com>
2025-11-13 09:37:34 +0900

    jsonの削除　カスタムバリデーション

--- Gemfile
+++ Gemfile
@@ -71,5 +71,3 @@ gem 'devise'
 
 gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
-
-gem 'validates_json'
--- app/models/whisky.rb
+++ app/models/whisky.rb
@@ -1,18 +1,24 @@
 class Whisky < ApplicationRecord
   belongs_to :user
 
-  # ActiveStorage
   has_one_attached :whisky_photo
   has_one_attached :glass_photo
 
-  # 定数として許可される香りの値を定義
   ALLOWED_AROMAS = %w[sweet citrus cream flower wine iodine peat].freeze
 
-  # バリデーション
   validates :whisky_name, :drink_style, presence: true
   validates :glass_rating, inclusion: { in: 1..5 }, allow_nil: true
-  validates :details, length: { maximum: 1000 }, allow_nil: true
+  validates :details, length: { maximum: 300 }, allow_nil: true
 
-  # JSONバリデーション（aromasがJSON配列かつ許可された値のみか）
-  validates :aromas, json: { schema: { type: "array", items: { enum: ALLOWED_AROMAS } } }, allow_nil: true
+  validate :aromas_must_be_array_of_allowed_values
+
+  private
+
+  def aromas_must_be_array_of_allowed_values
+    return if aromas.blank? # 任意なので空は許可
+
+    unless aromas.is_a?(Array) && aromas.all? { |a| ALLOWED_AROMAS.include?(a) }
+      errors.add(:aromas, "は正しい香りの配列である必要があります")
+    end
+  end
 end
