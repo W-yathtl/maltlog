71b8049177db5c10ff338c62610cf0c1c6f9dbea
W-yathtl <dopin9panda@gmail.com>
2025-11-13 09:34:20 +0900

    モデルの調整

--- app/models/whisky.rb
+++ app/models/whisky.rb
@@ -1,10 +1,18 @@
 class Whisky < ApplicationRecord
   belongs_to :user
+
+  # ActiveStorage
   has_one_attached :whisky_photo
   has_one_attached :glass_photo
 
+  # 定数として許可される香りの値を定義
+  ALLOWED_AROMAS = %w[sweet citrus cream flower wine iodine peat].freeze
+
+  # バリデーション
   validates :whisky_name, :drink_style, presence: true
   validates :glass_rating, inclusion: { in: 1..5 }, allow_nil: true
-  validates :aromas, json: true # 任意でJSONバリデーション（gem必要）
-end
+  validates :details, length: { maximum: 1000 }, allow_nil: true
+
+  # JSONバリデーション（aromasがJSON配列かつ許可された値のみか）
+  validates :aromas, json: { schema: { type: "array", items: { enum: ALLOWED_AROMAS } } }, allow_nil: true
 end
