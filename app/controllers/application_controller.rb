d6f4873fd135ed1fab29e11e76224cc3d4297bd1
W-yathtl <dopin9panda@gmail.com>
2025-12-29 13:01:56 +0900

    endコメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :basic_auth
+  before_action :basic_auth
 
    # ログアウト後にトップページに遷移させる
   def after_sign_out_path_for(resource_or_scope)
@@ -22,5 +22,5 @@ class ApplicationController < ActionController::Base
   #   authenticate_or_request_with_http_basic do |username, password|
   #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
   #   end
-  end
+  # end
 end
